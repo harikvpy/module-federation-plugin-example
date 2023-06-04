623864439a8d4f7cc32dd621291d6f5b62bae6de
Hari Mahadevan <hari@smallpearl.com>
2023-06-04 17:25:30 +0800

    Working static remote module

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,15 +1,22 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-
-  name: 'mfe1',
+  name: "mfe1",
 
   exposes: {
-    './Component': './projects/mfe1/src/app/app.component.ts',
+    // Update this whole line (both, left and right part):
+    "./Module": "./projects/mfe1/src/app/flights/flights.module.ts",
+    // './Component': './projects/mfe1/src/app/app.component.ts',
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
-
 });
