09234671c12e235bd5d04354b283d52989bbd265
Hari Mahadevan <hari@smallpearl.com>
2023-06-05 15:15:57 +0800

    Completed tutorial

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,22 +1,41 @@
+// Import share instead of shareAll:
 const {
-  shareAll,
+  share,
   withModuleFederationPlugin,
-} = require("@angular-architects/module-federation/webpack");
+} = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-  name: "mfe1",
+  name: 'mfe1',
 
   exposes: {
     // Update this whole line (both, left and right part):
-    "./Module": "./projects/mfe1/src/app/flights/flights.module.ts",
-    // './Component': './projects/mfe1/src/app/app.component.ts',
+    './Module': './projects/mfe1/src/app/flights/flights.module.ts',
   },
 
-  shared: {
-    ...shareAll({
+  // Explicitly share packages:
+  shared: share({
+    '@angular/core': {
       singleton: true,
       strictVersion: true,
-      requiredVersion: "auto",
-    }),
-  },
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+  }),
+
+  // Explicitly share mono-repo libs:
+  sharedMappings: ['auth-lib'],
 });
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,5 +1,5 @@
 const {
-  shareAll,
+  share,
   withModuleFederationPlugin,
 } = require("@angular-architects/module-federation/webpack");
 
@@ -8,11 +8,30 @@ module.exports = withModuleFederationPlugin({
     // mfe1: "http://localhost:4201/remoteEntry.js",
   },
 
-  shared: {
-    ...shareAll({
+  // Explicitly share packages:
+  shared: share({
+    "@angular/core": {
       singleton: true,
       strictVersion: true,
       requiredVersion: "auto",
-    }),
-  },
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common/http": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+  }),
+
+  // Explicitly share mono-repo libs:
+  sharedMappings: ["auth-lib"],
 });
